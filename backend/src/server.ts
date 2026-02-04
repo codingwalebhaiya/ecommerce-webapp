b6dba8ef986140a09764264e972a4275e07df3ec
codingwalebhaiya <moneymind144@gmail.com>
2026-02-04 23:43:46 +0530

    feat(db): add MongoDB database connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-dotenv.config();  
+dotenv.config();
 
-const port = process.env.PORT || 3000;  
+connectDB();
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-  
