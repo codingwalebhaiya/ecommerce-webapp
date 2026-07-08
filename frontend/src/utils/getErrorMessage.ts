46a071988e322f9083fa4c65426851d3054cea8d
codingwalebhaiya <moneymind144@gmail.com>
2026-07-08 10:53:14 +0530

    add error response utils

---
+++ frontend/src/utils/getErrorMessage.ts
@@ -0,0 +1,18 @@
+
+import { AxiosError } from "axios";
+
+export function getErrorMessage(error: unknown): string {
+  if (error instanceof AxiosError) {
+    return (
+      error.response?.data?.message ||
+      error.message ||
+      "Something went wrong"
+    );
+  }
+
+  if (error instanceof Error) {
+    return error.message;
+  }
+
+  return "Something went wrong";
+}
